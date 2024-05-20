ab3d87f090017f0e0bd003d68e11ea95521e6f77
Carlos Munoz Kampff <carlos@fountain-city.com>
2024-05-20 10:25:59 -0700

    fix instagram iframe issues.

--- app/page.tsx
+++ app/page.tsx
@@ -121,8 +121,8 @@ export default function Home() {
               className="instagram-media instagram-media-rendered"
               id="instagram-embed-0"
               src="https://www.instagram.com/p/BwP2jEWh3hN/embed/captioned/?cr=1&amp;v=12&amp;wp=326&amp;rd=https%3A%2F%2F21stcenturypizza.com&amp;rp=%2F#%7B%22ci%22%3A0%2C%22os%22%3A2196.7999999970198%2C%22ls%22%3A1405.699999988079%2C%22le%22%3A2193.2999999970198%7D"
-              allowTransparency="true"
-              allowFullScreen="true"
+              allowTransparency={true}
+              allowFullScreen={true}
               frameBorder="0"
               height="818"
               data-instgrm-payload-id="instagram-media-payload-0"
