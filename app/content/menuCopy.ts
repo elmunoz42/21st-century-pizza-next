13eac25c48186db2ad6bb5af8db5c53b14b705d2
Carlos Munoz Kampff <carlos@fountain-city.com>
2024-11-12 21:43:15 -0800

    add menu page content based on home page product section.

---
+++ app/content/menuCopy.ts
@@ -0,0 +1,9 @@
+export const MENU_COPY = [
+  {
+    title: "21ST CENTURY PIZZA",
+    subtitle: "Farm to table pizza at your doorstep.",
+    buttonText: "ORDER ONLINE",
+    buttonLink: "https://21stcenturypizza.com/order-online/",
+    accentMessage: "CALL: +1(503)954-1570",
+  },
+];
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next";
+import { ProductTableSection } from "../components/molecules/ProductTableSection";
+import { PRODUCTS } from "../content/products";
+import { MENU_COPY } from "../content/menuCopy"; 
 
 const Menu: NextPage = () => {
-  return <h1>Menu</h1>;
+  return (
+    <div className="container mx-auto">
+      <ProductTableSection products={PRODUCTS} copy={MENU_COPY[0]}/>
+    </div>
+  );
 };
 
 export default Menu;
